48fedb2dc2c2dd5c67dc0caf1a3284499ac5c82b
Welber Moraes <42919773+welberluanm@users.noreply.github.com>
2024-10-15 09:56:20 -0300

    Add files via upload
    novo

---
+++ SITE/js/script.js
@@ -0,0 +1,12 @@
+<!-- JavaScript -->
+    <script>
+        // Função para exibir o temporizador depois de 10 segundos
+        setTimeout(function() {
+            document.getElementById("timer").style.display = "block";
+        }, 10000);
+
+        // Função para exibir o pop-up de visitantes da semana
+        window.onload = function() {
+            alert("Número de visitantes na última semana: 250");
+        };
+    </script>
